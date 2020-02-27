ffe65fd24aa88ee3f91509a9c02d5d0e360ad8b5
CodHeK <gaganganapathyas@yahoo.in>
2020-02-27 11:34:23 +0800

    loaded & processed transcript to display

--- src/Components/Screens/Editor.js
+++ src/Components/Screens/Editor.js
@@ -16,6 +16,7 @@ const WaveForm = props => {
     let playlist = null;
 
     setTimeout(() => {
+        console.log('init');
         playlist = WaveformPlaylist.init(
             {
                 container: document.getElementById('waveform-playlist-container'),
@@ -28,8 +29,8 @@ const WaveForm = props => {
                     fadeColor: 'black',
                 },
                 annotationList: {
-                    annotations: notes,
-                    controls: actions,
+                    annotations: props.notes,
+                    controls: [],
                     editable: true,
                     isContinuousPlay: false,
                     linkEndpoints: true,
@@ -40,6 +41,7 @@ const WaveForm = props => {
     }, 500);
 
     setTimeout(() => {
+        console.log('load');
         playlist
             .load([
                 {
@@ -50,13 +52,15 @@ const WaveForm = props => {
                 //can do stuff with the playlist.
                 console.log('done!');
             });
-    }, 1000);
+    }, 500);
 
     return <></>;
 };
 
 const Editor = props => {
     const [transcriptionId, setTranscriptionId] = useState(null);
+    const [transcript, setTranscript] = useState(null);
+    const [filePath, setFilePath] = useState(null);
 
     useEffect(() => {
         let _id = null;
@@ -70,7 +74,55 @@ const Editor = props => {
         }
 
         setTranscriptionId(_id);
-    }, []);
+    }, [props._id]);
+
+    useEffect(() => {
+        const processSentances = sentences => {
+            let notes = [],
+                counter = 1;
+            for (let s of sentences) {
+                let startime = s.startTime;
+                let endTime = s.startTime + s.duration;
+
+                let lines = s.words.reduce((sentence, word) => {
+                    return sentence + ' ' + word.text;
+                }, '');
+
+                notes.push({
+                    begin: `${startime}`,
+                    end: `${endTime}`,
+                    id: `${counter}`,
+                    language: s.language,
+                    lines,
+                });
+
+                counter++;
+            }
+            return notes;
+        };
+
+        if (transcriptionId !== null) {
+            const URL = `${process.env.REACT_APP_API_HOST}/api/speech/${transcriptionId}`;
+            const token = localStorage.getItem('token');
+
+            fetch(URL, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then(res => res.json())
+                .then(data => {
+                    const { uploadedFile, sentences } = data.speech;
+
+                    const notes = processSentances(sentences);
+
+                    setFilePath(uploadedFile.path);
+                    setTranscript(notes);
+                });
+        }
+    }, [transcriptionId]);
 
     return (
         <React.Fragment>
@@ -114,7 +166,7 @@ const Editor = props => {
                             </div>
                         </div>
                         <div id="waveform-playlist-container"></div>
-                        <WaveForm _id={transcriptionId} />
+                        {transcript && <WaveForm _id={transcriptionId} notes={transcript} />}
                     </div>
                 </React.Fragment>
             )}
@@ -174,129 +226,6 @@ var actions = [
         action: (annotation, i, annotations) => {
             annotations.splice(i, 1);
         },
-    },
-];
-
-const notes = [
-    {
-        begin: '0.000',
-        children: [],
-        end: '2.680',
-        id: 'f000001',
-        language: 'eng',
-        lines: ['1'],
-    },
-    {
-        begin: '2.680',
-        children: [],
-        end: '5.880',
-        id: 'f000002',
-        language: 'eng',
-        lines: ['From fairest creatures we desire increase,'],
-    },
-    {
-        begin: '5.880',
-        children: [],
-        end: '9.240',
-        id: 'f000003',
-        language: 'eng',
-        lines: ["That thereby beauty's rose might never die,"],
-    },
-    {
-        begin: '9.240',
-        children: [],
-        end: '11.920',
-        id: 'f000004',
-        language: 'eng',
-        lines: ['But as the riper should by time decease,'],
-    },
-    {
-        begin: '11.920',
-        children: [],
-        end: '15.280',
-        id: 'f000005',
-        language: 'eng',
-        lines: ['His tender heir might bear his memory:'],
-    },
-    {
-        begin: '15.280',
-        children: [],
-        end: '18.600',
-        id: 'f000006',
-        language: 'eng',
-        lines: ['But thou contracted to thine own bright eyes,'],
-    },
-    {
-        begin: '18.600',
-        children: [],
-        end: '22.800',
-        id: 'f000007',
-        language: 'eng',
-        lines: ["Feed'st thy light's flame with self-substantial fuel,"],
-    },
-    {
-        begin: '22.800',
-        children: [],
-        end: '25.680',
-        id: 'f000008',
-        language: 'eng',
-        lines: ['Making a famine where abundance lies,'],
-    },
-    {
-        begin: '25.680',
-        children: [],
-        end: '31.240',
-        id: 'f000009',
-        language: 'eng',
-        lines: ['Thy self thy foe, to thy sweet self too cruel:'],
-    },
-    {
-        begin: '31.240',
-        children: [],
-        end: '34.280',
-        id: 'f000010',
-        language: 'eng',
-        lines: ["Thou that art now the world's fresh ornament,"],
-    },
-    {
-        begin: '34.280',
-        children: [],
-        end: '36.960',
-        id: 'f000011',
-        language: 'eng',
-        lines: ['And only herald to the gaudy spring,'],
-    },
-    {
-        begin: '36.960',
-        children: [],
-        end: '40.680',
-        id: 'f000012',
-        language: 'eng',
-        lines: ['Within thine own bud buriest thy content,'],
-    },
-    {
-        begin: '40.680',
-        children: [],
-        end: '44.560',
-        id: 'f000013',
-        language: 'eng',
-        lines: ["And tender churl mak'st waste in niggarding:"],
-    },
-    {
-        begin: '44.560',
-        children: [],
-        end: '48.080',
-        id: 'f000014',
-        language: 'eng',
-        lines: ['Pity the world, or else this glutton be,'],
-    },
-    {
-        begin: '48.080',
-        children: [],
-        end: '53.240',
-        id: 'f000015',
-        language: 'eng',
-        lines: ["To eat the world's due, by the grave and thee."],
     },
 ];
 
